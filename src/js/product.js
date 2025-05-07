d1d68c029a8b07d9fbeedc6bc85f35d644b8ba20
Dallin Hale <dallinhale@gmail.com>
2025-05-06 18:42:41 -0600

    dh--fixing/team

--- src/js/product.js
+++ src/js/product.js
@@ -1,26 +1,5 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import getProductDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart");
-  
-  if (cart === null) {
-  cart = [];
-  }
-  else if (!Array.isArray(cart)) {
-  cart = [cart];
-  }
-
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+getProductDetails(productId);
---
+++ src/js/productDetails.mjs
@@ -0,0 +1,45 @@
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
+let product = {};
+
+export default async function getProductDetails(productId) {
+  product = await findProductById(productId);
+  console.log(`Getting ${productId}`);
+  renderProductDetails();
+
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", function (event) {
+      addProductToCart(product);
+      console.log(`product ${productId} successfully added`);
+      console.log("Cart contents:", JSON.stringify(getLocalStorage("so-cart")));
+    });
+}
+
+function renderProductDetails() {
+  document.getElementById("productBrand").textContent = product.Brand.Name;
+  document.getElementById("productNameWithoutBrand").textContent =
+    product.NameWithoutBrand;
+  document.getElementById("productImage").src = product.Image;
+  document.getElementById("productImage").alt = product.NameWithoutBrand;
+  document.getElementById("productFinalPrice").textContent =
+    `${product.FinalPrice}`;
+  document.getElementById("productColorName").textContent =
+    product.Colors[0].ColorName;
+  document.getElementById("productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.getElementById("addToCart").setAttribute("data-id", product.Id);
+}
+
+function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart");
+
+  if (cart === null) {
+    cart = [];
+  } else if (!Array.isArray(cart)) {
+    cart = [cart];
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+}
