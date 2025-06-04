9534643543e14755f9e81eb595147fd1a4abaf65
LowanWill <lowannwil@byui.edu>
2025-06-03 19:15:27 -0600

    Register banner

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { updateCartCounter } from "./cart.js";
 import { loadHeaderFooter } from "./utils.mjs";
+import { showRegistrationBanner } from "./registerBanner.js";
 
 loadHeaderFooter().then(() => {
   updateCartCounter();
+  showRegistrationBanner();
 });
