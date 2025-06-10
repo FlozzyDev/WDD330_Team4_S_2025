f8aa91e35b22666febef735d35c6e7162ce99d34
Dallin Hale <dallinhale@gmail.com>
2025-06-10 12:36:45 -0600

    Adding ability to sort product list

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -3,11 +3,17 @@ import { updateCartCounter } from "./cart.js";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 import { checkAlert } from "./alert.js";
 
+const category = getParam("category");
 loadHeaderFooter().then(() => {
   updateCartCounter();
+
+  const sortDropdown = document.getElementById("filter-dropdown");
+  productList(".product-list", category);
+
+  sortDropdown.addEventListener("change", function () {
+    const sortOrder = this.value;
+    productList(".product-list", category, sortOrder);
+    this.blur();
+  });
 });
-
-const category = getParam("category");
-productList(".product-list", category);
-
 checkAlert(); // call function to see if any alerts are present
